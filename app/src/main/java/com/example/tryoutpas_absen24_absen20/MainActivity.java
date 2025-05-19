01f3200795ff568b643f5d4810300d44f5f8b6ef
muyajid <kamuyazidkan@gmail.com>
2025-05-19 15:10:12 +0700

    Call API Fragment English Dan Spanish Setting Fragment

--- app/src/main/java/com/example/tryoutpas_absen24_absen20/MainActivity.java
+++ app/src/main/java/com/example/tryoutpas_absen24_absen20/MainActivity.java
@@ -7,18 +7,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottom_navigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottom_navigation = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame_layout, new FragmentHome())
+                .commit();
+
+        bottom_navigation.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                fragment = new FragmentHome();
+            } else if (itemId == R.id.nav_dashboard) {
+                fragment = new FragmentDashboard();
+            } else if (itemId == R.id.nav_notifications) {
+                fragment = new FragmentNotifications();
+            }
+
+            if (fragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frame_layout, fragment)
+                        .commit();
+                return true;
+            }
+            return false;
         });
     }
 }
